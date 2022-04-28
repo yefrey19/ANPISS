b1303690cc5c234073cb7c668d77a629cf58d826
xiomara vargas <kamarinez0@misena.edu.co>
2022-04-28 11:25:04 -0500

    septimo commit kevin

---
+++ javascript/modal4.js
@@ -0,0 +1,6 @@
+let registrar21 = document.querySelector(".Registrar");
+let modal21= document.querySelector(".modal21");
+
+registrar21.addEventListener('click', function(){
+modal21.style.visibility = "visible";
+})
