16341fa5944efa8626579b40193ccb1a0db7bc8e
xiomara vargas <kamarinez0@misena.edu.co>
2022-04-28 07:10:30 -0500

    quinto commit kevin

---
+++ javascript/modal2.js
@@ -0,0 +1,6 @@
+let registrar2 = document.getElementById ("Registrar2");
+let modal2= document.querySelector(".modal2");
+
+registrar2.addEventListener('click', function(){
+modal2.style.visibility = "visible";
+})
