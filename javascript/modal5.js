cd9882f177f49125d498a8a91e373d97c2d5f41b
kevin <kamarinez0@misena.edu.co>
2022-05-10 10:23:46 -0500

    noveno commit kevin

---
+++ javascript/modal5.js
@@ -0,0 +1,7 @@
+let registrar3 = document.querySelector(".Registrar");
+let modal5 = document.querySelector(".modal5"); 
+
+
+registrar3.addEventListener('click', function(){
+    modal5.style.visibility = "visible";
+    })
