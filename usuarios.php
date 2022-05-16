83418e8ac1755b555c9d1eb2e0734a382fd1fc05
Yefrey Peña <penaaceroyefrey@gmail.com>
2022-05-16 09:43:22 -0500

    commit crud final

--- usuarios.php
+++ usuarios.php
@@ -77,8 +77,25 @@ $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
                                     <td><?php echo $row['nombre']; ?>
                                     <?php echo $row['apellido']; ?></td>
                                     <td><?php echo $row['telefono']; ?></td>
-                                    <td><a href="editar.php?id=<?php echo $row['id']; ?>" class="btn btn-warning">Editar</a>
-                                    <a href="eliminar.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Eliminar</a></td>
+                                    <td><a href="ver.php?id=<?php echo $row['id']; ?>" ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="25" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                                        <circle cx="12" cy="12" r="2" />
+                                                                                        <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+                                                                                        </svg></a>
+                                    <a href="editar.php?id=<?php echo $row['id']; ?>"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="25" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="#7f5345" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                                    <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" />
+                                                                                    <path d="M16 5l3 3" />
+                                                                                    <path d="M9 7.07a7.002 7.002 0 0 0 1 13.93a7.002 7.002 0 0 0 6.929 -5.999" />
+                                                                                    </svg>
+                                    <a href="eliminar.php?id=<?php echo $row['id']; ?>"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="25" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                                        <line x1="4" y1="7" x2="20" y2="7" />
+                                                                                        <line x1="10" y1="11" x2="10" y2="17" />
+                                                                                        <line x1="14" y1="11" x2="14" y2="17" />
+                                                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                                                                        </svg></a></td>
                                 </tr>
                             <?php } ?>
                         </tbody>
---
+++ ver.php
@@ -0,0 +1,116 @@
+<?php
+
+require 'conexion.php';
+
+$db = new Database();
+$con = $db->conectar();
+
+$id = $_GET['id'];
+$activo = 1;
+
+$query = $con->prepare("SELECT * FROM tblusuario WHERE id = :id AND activo=:activo");
+$query->execute(['id' => $id, 'activo' => $activo]);
+$num = $query->rowCount();
+if ($num > 0) {
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+} else {
+    header("Location: usuarios.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/estilos.css">
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+    <link rel="preload" href="css/normalize.css" as="styles">
+</head>
+<body>
+    <div class="busqueda">
+        <div class="search">
+            <form action="#">
+                <input type="text"
+                    placeholder=" Search Courses"
+                    name="search">
+                <button>
+                    <i class="fa fa-search"
+                        style="font-size: 18px;">
+                    </i>
+                </button>
+            </form>
+        </div>
+    </div>
+    
+       <a href="Principal.html" target="Anpiss"><svg xmlns="http://www.w3.org/2000/svg" class=" log-usua icon icon-tabler icon-tabler-text-wrap" width="56" height="56" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="18" x2="9" y2="18" />
+            <path d="M4 12h13a3 3 0 0 1 0 6h-4l2 -2m0 4l-2 -2" />
+        </svg></a>
+    <main class="contenedor">
+        <div class="p-3 rounded">
+            <div class="row">
+                <div class="col">
+                    <h4>Editar usuario</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <form class="row g-3" method="POST" action="" autocomplete="off">
+                        <input type="hidden" id="id" name="id" value="<?php echo $id; ?>">
+                        <div class="col-md-4">
+                            <label for="codigo" class="form-label">Nombre</label>
+                            <input type="text" id="codigo" name="nombre" class="form-control" value="<?php echo $row['nombre']; ?>" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="descripcion" class="form-label">Apellido</label>
+                            <input type="text" id="descripcion" name="apellido" class="form-control" value="<?php echo $row['apellido']; ?>" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="stock" class="form-label">Documento</label>
+                            <input type="number" id="stock" name="documento" value="<?php echo $row['documento']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="correo" class="form-label">Correo</label>
+                            <input type="email" id="correo" name="correo" value="<?php echo $row['correo']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="stock" class="form-label">Telefono</label>
+                            <input type="number" id="stock" name="telefono" value="<?php echo $row['telefono']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="stock" class="form-label">Edad</label>
+                            <input type="number" id="stock" name="edad" value="<?php echo $row['edad']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="stock" class="form-label">Cargo</label>
+                            <input type="text" id="stock" name="cargo" value="<?php echo $row['cargo']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="col-md-12">
+                            <a class="btn btn-secondary" href="usuarios.php">Regresar</a>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
