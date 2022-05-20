0517a9c725711bf3dc656339b34738ecfe9ecdaf
xiomara vargas <kamarinez0@misena.edu.co>
2022-05-20 10:17:35 -0500

    16 commit

--- DocLimpieza.php
+++ DocLimpieza.php
@@ -6,11 +6,9 @@ $db = new Database();
 $con = $db->conectar();
 
 $comando = $con->prepare("SELECT * FROM tbllimpieza ");
-
-<<<<<<< HEAD
 $comando = $con->prepare("SELECT * FROM tbllimpieza ");
-=======
->>>>>>> d4eecb60679e9be396e8e0a8d37bec0fbc7c3f73
+
+
 $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
